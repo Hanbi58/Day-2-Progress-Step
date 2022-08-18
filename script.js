21715de9fa1592698484d50aac918a7a077572d2
Hanbi58 <hanxiaonan2012@gmail.com>
2022-08-18 16:15:57 -0700

    teacher

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-alert('1')
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+  currentActive > circles.length && (currentActive = circles.length);
+  console.log(currentActive);
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  currentActive < 1 && (currentActive = 1);
+  console.log(currentActive);
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    idx < currentActive
+      ? circle.classList.add("active")
+      : circle.classList.remove("active");
+  });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    prev.disabled = ture;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
